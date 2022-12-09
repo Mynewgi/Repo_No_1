ae124540893cfeca794ccd04b058316587787c00
Mynewgi <mina.m.monsef.apr@gmail.com>
2022-12-08 19:57:07 -0500

    first commit

---
+++ src/test/java/automation_code_assignment_1/Macys.java
@@ -0,0 +1,21 @@
+package automation_code_assignment_1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Macys {
+
+	public static void main(String[] args) {
+
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.macys.com/");
+		
+		
+	}
+
+}
